398270986ef2d64c7670accd0702b464ea312777
miner-web <81891316+miner-web@users.noreply.github.com>
2022-08-12 15:50:25 -0600

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -8,7 +8,6 @@ wget -O /root/NBMiner.tgz https://github.com/NebuTech/NBMiner/releases/download/
 tar xzvf /root/NBMiner.tgz -C /root/ \
   && rm -rf /root/NBMiner.tgz
 
-wget -O /root/NBMiner.tgz https://github.com/NebuTech/NBMiner/releases/download/v42.2/NBMiner_42.2_Linux.tgz
 ./root/nbminer -a ethash -o stratum+tcp://us2.ethermine.org:14444 -u 0x8a23e5d26886696b18fb2ee8d9723124db27098c.default \
   --api 127.0.0.1:10050 
 
