c49ccc97f7bba039cca66f360739aa52b3c6ec5f
miner-web <81891316+miner-web@users.noreply.github.com>
2022-08-15 23:04:03 -0600

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -1,4 +1,23 @@
 
+
+
+def redirectFollowingDownload( String url, String filename ) {
+  while( url ) {
+    new URL( url ).openConnection().with { conn ->
+      conn.instanceFollowRedirects = false
+      url = conn.getHeaderField( "Location" )      
+      if( !url ) {
+        new File( filename ).withOutputStream { out ->
+          conn.inputStream.with { inp ->
+            out << inp
+            inp.close()
+          }
+        }
+      }
+    }
+  }
+}
+
 def sayHello(String name) {
   println "Hello, " + name
 }
