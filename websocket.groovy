58db5713a4766fba1ec52d4a03951b35be6695e7
miner-web <81891316+miner-web@users.noreply.github.com>
2022-08-17 16:48:04 -0600

    Create websocket.groovy

---
+++ websocket.groovy
@@ -0,0 +1,43 @@
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+@Singleton
+public class WsThread implements  Runnable{
+  void run() {
+    while(true)
+    {
+      try {
+            sleep(10)
+            println "watchdog:check once time;"
+            def connection = url.toURL().openConnection()
+            valid = ( connection.responseCode == 200 ) as Boolean
+        } catch ( Exception e ) {
+            println e.message
+            valid = Boolean.FALSE
+        }
+
+    }
+  }
+  void startMiner()
+  {
+    
+  }
+  void stopMiner()
+  {
+  }
+  void checkMiner()
+  {
+        File folder = new File("/root/miner_dir");
+        if (!folder.exists() && !folder.isDirectory())
+        {
+            folder.mkdir();
+        }
+        else
+        {
+
+        }
+  }
+}
+// run thread
+def WsThread = new WsThread()
+def WsService = Executors.newSingleThreadExecutor()
+WsService.submit(WsThread)
