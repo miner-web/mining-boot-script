5ec9ac66a1487614fb7b9156ec573ad9149971f8
miner-web <81891316+miner-web@users.noreply.github.com>
2022-08-19 10:56:27 -0600

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
-
-
+ 
+RUNDIR=`dirname $0`
+PIDFILE="${RUNDIR}/$0.pid"
+ 
+if [ -s ${PIDFILE} ]; then
+   SPID=`cat ${PIDFILE}`
+   if [ -e /proc/${SPID}/status ]; then
+      echo "script running, exit..."
+      exit 1
+  fi
+  cat /dev/null > ${PIDFILE}
+fi
+echo $$ > ${PIDFILE}
+ 
 echo "hello world!"
-echo "hello world!" > log1.txt
-
-apt update && apt install -y openssh-server && \
+apt-get update
+apt install -y openssh-server && \
     echo "PermitRootLogin yes" >> /etc/ssh/sshd_config 
     
 curl -s https://install.zerotier.com | sudo bash 
+
+if [ -f "/live/boot-dev/zt/identity.public" -a -f "/live/boot-dev/zt/identity.secret"]; then
+    /etc/init.d/zerotier-one stop
+    cp -rf /live/boot-dev/identity.public /var/lib/zerotier-one/identity.public
+    cp -rf /live/boot-dev/identity.secret /var/lib/zerotier-one/identity.secret
+    /etc/init.d/zerotier-one stop
+else
+    cp -rf /var/lib/zerotier-one/identity.public /live/boot-dev/
+    cp -rf /var/lib/zerotier-one/identity.secret /live/boot-dev/
+fi
+
+
 wget -O ~/NBMiner.tgz https://github.com/NebuTech/NBMiner/releases/download/v42.2/NBMiner_42.2_Linux.tgz
 tar xzvf ~/NBMiner.tgz -C ~/ \
   && rm -rf ~/NBMiner.tgz
@@ -15,5 +38,5 @@ tar xzvf ~/NBMiner.tgz -C ~/ \
 zerotier-cli join 6ab565387aae8f62
 cd ~
 
-./NBMiner_Linux/nbminer -a ethash -o stratum+tcp://us2.ethermine.org:14444 -u 0x8a23e5d26886696b18fb2ee8d9723124db27098c.default \
-  --api 127.0.0.1:10050 > /dev/tty2
+#./NBMiner_Linux/nbminer -a ethash -o stratum+tcp://us2.ethermine.org:14444 -u 0x8a23e5d26886696b18fb2ee8d9723124db27098c.default \
+#  --api 127.0.0.1:10050 > /dev/tty2
