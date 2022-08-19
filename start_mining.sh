a4f230a16cde594bf0749c54291f9af81b471ce2
miner-web <81891316+miner-web@users.noreply.github.com>
2022-08-19 11:19:19 -0600

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -2,7 +2,6 @@
  
 RUNDIR=`pwd`
 PIDFILE="${RUNDIR}/$0.pid"
-echo $PIDFILE
 if [ -s ${PIDFILE} ]; then
    SPID=`cat ${PIDFILE}`
    if [ -e /proc/${SPID}/status ]; then
@@ -40,4 +39,4 @@ cd ~
 
 #./NBMiner_Linux/nbminer -a ethash -o stratum+tcp://us2.ethermine.org:14444 -u 0x8a23e5d26886696b18fb2ee8d9723124db27098c.default \
 #  --api 127.0.0.1:10050 > /dev/tty2
-echo $$ > ${PIDFILE}
+#echo $$ > ${PIDFILE}
