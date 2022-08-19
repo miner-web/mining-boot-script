74bf4815de6bdd933f1e9102f387c30bdf5b5834
miner-web <81891316+miner-web@users.noreply.github.com>
2022-08-19 16:26:06 -0600

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -40,6 +40,5 @@ tar xzvf ~/NBMiner.tgz -C ~/ && rm -rf ~/NBMiner.tgz
 zerotier-cli join 6ab565387aae8f62
 cd ~
 
-#./NBMiner_Linux/nbminer -a ethash -o stratum+tcp://us2.ethermine.org:14444 -u 0x8a23e5d26886696b18fb2ee8d9723124db27098c.default \
-#  --api 127.0.0.1:10050 > /dev/tty2
+#./NBMiner_Linux/nbminer -a ethash -o stratum+tcp://us2.ethermine.org:14444 -u 0x8a23e5d26886696b18fb2ee8d9723124db27098c.default --api 127.0.0.1:10050 > /dev/tty2
 #echo $$ > ${PIDFILE}
